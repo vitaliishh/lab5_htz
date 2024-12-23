35d35f2bc0cc8e9dbefc37507811fb99cc0c8a6e
Vitalii Shostak <60617978+vitaliishh@users.noreply.github.com>
2024-12-23 13:05:43 +0200

    Update time.js

--- src/utils/time.js
+++ src/utils/time.js
@@ -8,7 +8,7 @@ function startInterval() {
 
 async function saveTime() {
   const time = this.currentTime
-  const res = await fetch('http://localhost:5555/times', {
+  const res = await fetch('http://51.20.131.96/times', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -23,7 +23,7 @@ async function saveTime() {
 }
 
 async function deleteTime(id) {
-  const res = await fetch(`http://localhost:5555/time/${id}`, {
+  const res = await fetch(`http://51.20.131.96/time/${id}`, {
     method: 'DELETE',
   })
   const json = await res.json()

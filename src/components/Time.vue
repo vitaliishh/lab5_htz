ff175dcab621302032c9f89e222f71f74e917910
Vitalii Shostak <60617978+vitaliishh@users.noreply.github.com>
2024-12-23 13:10:13 +0200

    Update Time.vue

--- src/components/Time.vue
+++ src/components/Time.vue
@@ -19,7 +19,7 @@ export default {
   },
   created: async function () {
     this.startInterval()
-    const res = await fetch('http://localhost:5555/times')
+    const res = await fetch('http://51.20.131.96/times')
     const json = await res.json()
     if (json.length) this.savedTimes = json
   },
